21b6f41d81b08c98f7427ca7097784180a0f6d45
Emergent-code <88061315+emergent-code@users.noreply.github.com>
2023-12-03 18:07:26 +0000

    Added basic UI. No restart function

--- rock_paper_scissors_script.js
+++ rock_paper_scissors_script.js
@@ -6,74 +6,71 @@ function computerChoice(){
   return compChoice
 }
 
+const rockbtn = document.querySelector(".rock")
+const paperbtn = document.querySelector(".paper")
+const scissorsbtn = document.querySelector(".scissors")
+const scoreboard = document.querySelector(".scoreboard")
+let playerScore = 0
+let compScore = 0
+scoreboard.textContent = `${playerScore} - ${compScore}`
+
 //Game logic - plays a single round taking in the player's choice as input
-function gameRound(computer){
-  let compChoice = computer
-  let playerChoice = prompt("What is your choice?")
+//Will textContent of scoreboard change dynamically if it's outside of the function or does the update need to be included in the function? I suspect the latter
+function gameRound(playerChoice){
+  let compChoice = computerChoice()
   playerChoice = playerChoice.toLowerCase()
-  //let message = ""
- 
+
+
   if(compChoice == "rock" && playerChoice == "scissors" || compChoice == "paper" && playerChoice == "rock" || compChoice == "scissors" && playerChoice == "paper"){
-    //message = `Computer chose ${compChoice}. You lose :(`
-    return 1
+    
+    if(checkScore(playerScore, compScore)){
+      scoreboard.textContent = `Game over! Player: ${playerScore} \nComputer:${compScore}`
+    }
+    else{
+      compScore +=1
+      scoreboard.textContent = `${playerScore} - ${compScore}`
+    }
+    console.log("Lose")
+    console.log(`Player: ${playerScore}`)
+    console.log(`Computer: ${compScore}`)
   }
 
   else if(compChoice == "rock" && playerChoice == "paper" || compChoice == "scissors" && playerChoice == "rock" || compChoice == "paper" && playerChoice == "scissors"){
-    //message = `Computer chose ${compChoice}. You win!`
-    return 2
+
+    if(checkScore(playerScore, compScore)){
+      scoreboard.textContent = `Game over! Player: ${playerScore} \nComputer:${compScore}`
+    }
+    else{
+      playerScore +=1
+      scoreboard.textContent = `${playerScore} - ${compScore}`
+    }
+    console.log("Win")
+    console.log(`Player: ${playerScore}`)
+    console.log(`Computer: ${compScore}`)
   }
 
   else if (compChoice == "rock" && playerChoice == "rock" || compChoice == "scissors" && playerChoice == "scissors" || compChoice == "paper" && playerChoice == "paper"){
-    //message = `Computer also chose ${compChoice}. It's a draw!`
-    return 3
+    console.log("Draw")
+    console.log(`Player: ${playerScore}`)
+    console.log(`Computer: ${compScore}`)
+
   }
   else{
-    //message = `ERROR - in gameRound`
-    return 4
+    return console.log("ERROR")
   }
 }
 
-function fullGame(){
-  let message = ""
-  let score = 0
-  for(i = 0; i < 5; i++){
-    let compDecision = computerChoice()
-    let gameCondition = gameRound(compDecision)
-    if(gameCondition == 1){
-      message = `Computer chose ${compDecision}. You lose this round :(`
-      console.log(message)
-    }
-    else if(gameCondition == 2){
-      message = `Computer chose ${compDecision}. You win!`
-      console.log(message)
-      score += 1
-    }
-    else if(gameCondition == 3){
-      message = `Computer also chose ${compDecision}. It's a draw. Redo!`
-      console.log(message)
-      i --
-    }
-    else{
-      console.log("Oops, something went wrong!")
-    }
-    
-  }
-
-  let result = ""
-
-  if(score >= 3 ){
-    message = "Nice! You won the match!"
-    result = "won"
-  }
-  else if(score < 3){
-    message = "Oh no, you lost...Refresh to try again!"
-    result = "lost"
-  }
+function checkScore(playerS, compS){
+  if(playerS == 5 || compS ==5)
+    return true
   else{
-    console.log("If you're seeing this message, something went wrong lol")
+    return false
   }
 
-  console.log(`You won ${score} out of 5 rounds. ${message}`)
+
 }
 
-fullGame()
+rockbtn.addEventListener('click', () => {gameRound("rock")})
+paperbtn.addEventListener('click', () => {gameRound("paper")})
+scissorsbtn.addEventListener('click', () => {gameRound("scissors")})
+
